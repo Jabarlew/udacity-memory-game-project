1304e7abba1b338c2b736b0d27cacbb6925a3d53
Bartosz Lewandowski <barlewdesign@gmail.com>
2018-03-11 23:40:58 +0100

    before matching

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -1,22 +1,77 @@
+//ALWAYS ON START
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
+  ///array of images
+  const availableCardImages = [
+      'bear.png',
+      'elephant.png',
+      'giraffe.png',
+      'lion.png',
+      'mpalmer.png',
+      'panda.png',
+      'rabbit.png',
+      'rhino.png'
+    ]
+    ///creates path to images from array of images
+    .map(fileName => `img/${fileName}`);
 
-  const gameTime = 0;
-  let moves = 0;
-  const gameTime_h1 = document.querySelector('.score-moves-counter');
-  const cards = document.querySelectorAll('.item');
-  let openedCards = [];
 
-  function clickFunction() {
-    openedCards.push(this);
-    let count = openedCards.length;
-    if (count === 2) {
-      moves++
-      console.log(moves);
-      openedCards = [];
-    }
-  }
-  cards.forEach(function (element, index) {
-    cards[index].addEventListener('click', clickFunction);
-  });
+  function startGame() {
+    ///Duplicate and shuffle card images
+    const cardImages = _.shuffle(availableCardImages
+      ///loops through array and duplicate it
+      .reduce((acc, imgPath) => acc.concat([imgPath, imgPath]), []));
+
+    //creates array from node list.
+    const cards = Array.from(document.querySelectorAll('.board > li'))
+
+    ///maps through array and create objects
+      .map((li, i) => ({
+        element: li,
+        img: cardImages[i],
+      }));
+    //adds images
+    cards.forEach((card) => {
+      const img = card.element.querySelector('img');
+      img.src = card.img;
+
+      card.element.addEventListener('click', () => {
+        card.element.classList.add('selected');
+      });
+    });
+  };
+
+
+  startGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
