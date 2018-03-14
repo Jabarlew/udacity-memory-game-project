42ba37d8b0fe811afddf241c25f8441a0b28442e
Bartosz Lewandowski <barlewdesign@gmail.com>
2018-03-14 19:24:55 +0100

    fixed functions and white space still no other features

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -1,51 +1,52 @@
-//ALWAYS ON START
+// ALWAYS ON START
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
+
   let numberOfMoves = 0;
-  ///array of images
+  // /array of images
   const availableCardImages = [
-      'bear.png',
-      'elephant.png',
-      'giraffe.png',
-      'lion.png',
-      'mpalmer.png',
-      'panda.png',
-      'rabbit.png',
-      'rhino.png'
-    ]
-    ///creates path to images from array of images
+    'bear.png',
+    'elephant.png',
+    'giraffe.png',
+    'lion.png',
+    'mpalmer.png',
+    'panda.png',
+    'rabbit.png',
+    'rhino.png',
+  ]
+    // /creates path to images from array of images
     .map(fileName => `img/${fileName}`);
-  //keep track of active selections
+  // keep track of active selections
 
   let selectedCards = [];
 
   function startGame() {
-    ///Duplicate and shuffle card images
+    // /Duplicate and shuffle card images
     const cardImages = _.shuffle(availableCardImages
-      ///loops through array and duplicate it
+      // /loops through array and duplicate it
       .reduce((acc, imgPath) => acc.concat([imgPath, imgPath]), []));
 
-    //creates array from node list.
+    // creates array from node list.
     const cards = Array.from(document.querySelectorAll('.board > li'))
 
-      ///maps through array and create objects
+      // /maps through array and create objects
       .map((li, i) => ({
         element: li,
         img: cardImages[i],
       }));
-    //adds images
+    // adds images
     cards.forEach((card) => {
       const img = card.element.querySelector('img');
       img.src = card.img;
-//adds click event
+      // adds click event
       card.element.addEventListener('click', () => {
-//prevent ading more than 2 cards to selectedCards and dont allow to add 2 times the same card
+        // prevent ading more than 2 cards to selectedCards and dont allow to add 2 times the same card
         if (selectedCards.length < 2 && card.element.classList.contains('opened') === false) {
           selectedCards.push(card);
-          card.element.classList.add('opened')
-          card.element.classList.remove('wrong')
-          movesCounter();
+          card.element.classList.add('opened');
+          card.element.classList.remove('wrong');
           if (selectedCards.length === 2) {
+            numberOfMoves++;
             if (selectedCards[0].img === selectedCards[1].img) {
               card.element.classList.add('disabled');
               selectedCards = [];
@@ -57,38 +58,22 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     });
-  };
+  }
 
 
   startGame();
 
-
-
-
-function movesCounter () {
-  if (selectedCards.length === 2) {
-    numberOfMoves++
-  }
-};
-
-function openCards() {
-
-}
-
-function unselectCards() {
-  selectedCards.forEach((card) => {
+  function unselectCards() {
+    selectedCards.forEach((card) => {
       card.element.classList.remove('opened');
       card.element.classList.remove('wrong-card');
-
     });
     selectedCards = [];
-  };
+  }
 
-  function wrongCards() {
-  selectedCards.forEach((card) => {
-    card.element.classList.add('wrong-card');
-
+function wrongCards() {
+    selectedCards.forEach((card) => {
+      card.element.classList.add('wrong-card');
     });
-  };
-
+  }
 });
