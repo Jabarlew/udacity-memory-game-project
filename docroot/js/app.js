c8a5b889343cb79ba5b7e587e699a35c4b0bd947
Bartosz Lewandowski <barlewdesign@gmail.com>
2018-03-14 15:04:09 +0100

    preparing for clock

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -43,12 +43,14 @@ document.addEventListener('DOMContentLoaded', () => {
         if (selectedCards.length < 2 && card.element.classList.contains('opened') === false) {
           selectedCards.push(card);
           card.element.classList.add('opened')
+          card.element.classList.remove('wrong')
           movesCounter();
           if (selectedCards.length === 2) {
             if (selectedCards[0].img === selectedCards[1].img) {
               card.element.classList.add('disabled');
               selectedCards = [];
             } else {
+              setTimeout(wrongCards, 500);
               setTimeout(unselectCards, 1000);
             }
           }
@@ -76,8 +78,17 @@ function openCards() {
 function unselectCards() {
   selectedCards.forEach((card) => {
       card.element.classList.remove('opened');
+      card.element.classList.remove('wrong-card');
+
     });
     selectedCards = [];
+  };
+
+  function wrongCards() {
+  selectedCards.forEach((card) => {
+    card.element.classList.add('wrong-card');
+
+    });
   };
 
 });
