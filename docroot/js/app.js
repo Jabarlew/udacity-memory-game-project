059ca1bbc924dcc90dea2c56b78f83c50b44c45c
Bartosz Lewandowski <barlewdesign@gmail.com>
2018-03-14 12:36:17 +0100

    working game

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -36,21 +36,21 @@ document.addEventListener('DOMContentLoaded', () => {
     cards.forEach((card) => {
       const img = card.element.querySelector('img');
       img.src = card.img;
-
+//adds click event
       card.element.addEventListener('click', () => {
-        selectedCards.push(card);
-        card.element.classList.add('opened')
-        console.log(selectedCards);
-        if (selectedCards.length === 2) {
-          if (selectedCards[0].img === selectedCards[1].img) {
-            card.element.classList.add('disabled');
-            selectedCards = [];
-          } else {
-            setTimeout(unselectCards, 1100);
+//prevent ading more than 2 cards to selectedCards and dont allow to add 2 times the same card
+        if (selectedCards.length < 2 && card.element.classList.contains('opened') === false) {
+          selectedCards.push(card);
+          card.element.classList.add('opened')
+          console.log(selectedCards); ///remove later
+          if (selectedCards.length === 2) {
+            if (selectedCards[0].img === selectedCards[1].img) {
+              card.element.classList.add('disabled');
+              selectedCards = [];
+            } else {
+              setTimeout(unselectCards, 1000);
+            }
           }
-        }
-        if (selectedCards >= 2) {
-          selectedCards = [];
         }
       });
     });
@@ -61,36 +61,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+function openCards() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
   function unselectCards() {
-    selectedCards.forEach((card) => {
+  selectedCards.forEach((card) => {
       card.element.classList.remove('opened');
     });
     selectedCards = [];
   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
