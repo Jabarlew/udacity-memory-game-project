6d77b84e7c8736073eaa4569f908bbcfef7a52d8
Bartosz Lewandowski <barlewdesign@gmail.com>
2018-03-21 21:05:55 +0100

    stars problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,6 @@ module.exports = {
     es6: true,
   },
   rules: {
-    strict: ['error', 'global'],
+    strict: ['error', 'function'],
   },
 };
--- docroot/js/app.js
+++ docroot/js/app.js
@@ -11,15 +11,15 @@ document.addEventListener('DOMContentLoaded', () => {
   let numberOfMoves = 0;
   // /array of images
   const availableCardImages = [
-      'badger.svg',
-      'beawer.svg',
-      'bull.svg',
-      'cat.svg',
-      'hiena.svg',
-      'moose.svg',
-      'panthera.svg',
-      'zebra.svg',
-    ]
+    'badger.svg',
+    'beawer.svg',
+    'bull.svg',
+    'cat.svg',
+    'hiena.svg',
+    'moose.svg',
+    'panthera.svg',
+    'zebra.svg',
+  ]
     // /creates path to images from array of images
     .map(fileName => `img/${fileName}`);
   // keep track of active selections
@@ -41,18 +41,96 @@ document.addEventListener('DOMContentLoaded', () => {
         img: cardImages[i],
       }));
     // adds images
+    function unselectCards() {
+      selectedCards.forEach((card) => {
+        card.element.classList.remove('opened');
+        card.element.classList.remove('wrong-card');
+      });
+      selectedCards = [];
+    }
+
+    function endGame() {
+      clearInterval(interval);
+      const endText = document.querySelector('.message-text');
+      const hide = document.querySelector('.screen');
+
+      hide.classList.remove('hide');
+      document.body.classList.add('win');
+
+      endText.innerHTML = `You made ${numberOfMoves} moves. Your time is ${hours} h ${minutes} m ${seconds} s `;
+    }
+
+    function matchedCards() {
+      selectedCards.forEach((card) => {
+        card.element.classList.add('disabled');
+        card.element.classList.add('matched');
+        if (document.querySelectorAll('.matched').length === 16) endGame();
+      });
+      selectedCards = [];
+    }
+
+
+    function wrongCards() {
+      selectedCards.forEach((card) => {
+        card.element.classList.add('wrong-card');
+      });
+    }
+
+    function startTimer() {
+      function tick() {
+        time.innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+        seconds += 1;
+        if (seconds === 60) {
+          minutes += 1;
+          seconds = 0;
+        }
+        if (minutes === 60) {
+          hours += 1;
+          minutes = 0;
+        }
+      }
+
+      if (numberOfMoves === 1) {
+        hours = 0;
+        seconds = 0;
+        minutes = 0;
+        interval = setInterval(tick, 1000);
+      }
+    }
+
+    function grade() {
+      const starOne = document.querySelector('.grade-star-one');
+      const starTwo = document.querySelector('.grade-star-two');
+
+      if (numberOfMoves === 14) {
+        starOne.classList.remove('fas');
+        starOne.classList.add('far');
+      }
+      if (numberOfMoves === 17) {
+        starTwo.classList.remove('fas');
+        starTwo.classList.add('far');
+      }
+    }
+
+    document.querySelector('.reset').addEventListener('click', () => {
+      document.location.reload();
+    });
+    document.querySelector('.message-play-again').addEventListener('click', () => {
+      document.location.reload();
+    });
+
     cards.forEach((card) => {
       const img = card.element.querySelector('img');
       img.src = card.img;
       // adds click event
       card.element.addEventListener('click', () => {
-        // prevent ading more than 2 cards to selectedCards and dont allow to add 2 times the same card
+        // cant 2 cards to selectedCards and dont allow to add 2 times the same card
         if (selectedCards.length < 2 && card.element.classList.contains('opened') === false) {
           selectedCards.push(card);
           card.element.classList.add('opened');
           card.element.classList.remove('wrong');
           if (selectedCards.length === 2) {
-            numberOfMoves++;
+            numberOfMoves += 1;
             grade();
             startTimer();
             h1moves.innerHTML = numberOfMoves;
@@ -68,79 +146,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
   startGame();
-
-  function unselectCards() {
-    selectedCards.forEach((card) => {
-      card.element.classList.remove('opened');
-      card.element.classList.remove('wrong-card');
-    });
-    selectedCards = [];
-  }
-
-  function matchedCards() {
-    selectedCards.forEach((card) => {
-      card.element.classList.add('disabled');
-      card.element.classList.add('matched');
-      endGame();
-    });
-    selectedCards = [];
-  }
-
-  function wrongCards() {
-    selectedCards.forEach((card) => {
-      card.element.classList.add('wrong-card');
-    });
-  }
-
-  function timer() {
-    interval = setInterval(function () {
-      time.innerHTML = hours + ' h ' + minutes + ' m ' + seconds + ' s';
-      seconds++;
-      if (seconds === 60) {
-        minutes++;
-        seconds = 0;
-      }
-      if (minutes === 60) {
-        hours++;
-        minutes = 0;
-      }
-    }, 1000);
-  }
-
-  function startTimer() {
-    if (numberOfMoves === 1) {
-      hours = 0;
-      seconds = 0;
-      minutes = 0;
-      timer();
-    }
-  }
-
-  function grade() {
-    const starOne = document.querySelector('.grade-star-one');
-    const starTwo = document.querySelector('.grade-star-two');
-
-    if (numberOfMoves === 14) {
-      starOne.classList.remove('fas');
-      starOne.classList.add('far');
-    }
-    if (numberOfMoves === 17) {
-      starTwo.classList.remove('fas');
-      starTwo.classList.add('far');
-    }
-  }
-
-  function endGame() {
-    let win = document.querySelectorAll('.matched');
-    const endText = document.querySelector('.message-text');
-    const hide = document.querySelector('.screen');
-    if (win.length === 16) {
-      clearInterval(interval);
-      hide.classList.remove('hide');
-      document.body.classList.add('win');
-      endText.innerHTML = 'You made ' + numberOfMoves + ' moves. Your time is ' + hours + ' h ' + minutes + ' m ' + seconds + ' s ';
-    }
-
-  }
-
 });
